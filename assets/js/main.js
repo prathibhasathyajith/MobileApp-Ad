484657f500003a029116fe2a1706229c8304e9b2
Prathibha Sathyajith <prathibha4you@gmail.com>
2017-10-26 16:02:49 +0530

    edited
    add images
    add motions

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    var movementStrength = 25;
+    var height = movementStrength / $(window).height();
+    var width = movementStrength / $(window).width();
+    $("body").mousemove(function (e) {
+        var pageX = e.pageX - ($(window).width() / 2);
+        var pageY = e.pageY - ($(window).height() / 2);
+        var newvalueX = width * pageX * -1 - 50;
+        var newvalueY = height * pageY * -1 - 165;
+
+        var newvalueXC3 = width * pageX * -1 + 50;
+        var newvalueYC3 = height * pageY * -1 + 0;
+
+        var newvalueXC1 = width * pageX - 330;
+        var newvalueYC1 = height * pageY - 800;
+        
+        var mPointX = e.pageX*5/100;
+        var mPointY = e.pageY*15/100;
+    
+
+        $('#top-image').css("background-position", newvalueX + "px  " + newvalueY + "px");
+        $('#top-image-cloud3').css("background-position", -newvalueXC3 + "px  " + -newvalueYC3 + "px");
+        $('#top-image-cloud1').css("background-position", (750+mPointX) + "px  " + (mPointY+300) + "px");
+
+    });
+});
