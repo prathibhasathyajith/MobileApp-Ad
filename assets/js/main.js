4eba4e1d4856f9a0149ca388e249b5c88d948a4e
Prathibha Sathyajith <prathibha4you@gmail.com>
2017-10-27 14:45:05 +0530

    edit
    add animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,26 +13,38 @@ $(document).ready(function () {
 
         var newvalueXC1 = width * pageX - 330;
         var newvalueYC1 = height * pageY - 800;
-        
-        var mPointX = e.pageX*5/100;
-        var mPointY = e.pageY*15/100;
-    
+
+        var mPointX = e.pageX * 5 / 100;
+        var mPointY = e.pageY * 15 / 100;
+
 
         $('#top-image').css("background-position", newvalueX + "px  " + newvalueY + "px");
         $('#top-image-cloud3').css("background-position", -newvalueXC3 + "px  " + -newvalueYC3 + "px");
-        $('#top-image-cloud1').css("background-position", (750+mPointX) + "px  " + (mPointY+300) + "px");
+        $('#top-image-cloud1').css("background-position", (750 + mPointX) + "px  " + (mPointY + 300) + "px");
 
     });
-    $(".mobile-interface").mousemove(function (e){
-         e.stopPropagation();
+    $(".mobile-interface").click(function () {
+        $(".mobile-interface").addClass("animate");
+        //        $(".mobile-interface").animate({transform: "scale(1)"},"slow");
+        setTimeout(function () {
+            $(".mobile-interface2").css("box-shadow", "0px 0px 20px 11px rgba(0, 0, 0, 0.32)");
+            $(".mobile-interface2").css("left", "25vw");
+        }, 300);
+
+        setTimeout(function () {
+            $(".mobile-interface3").css("box-shadow", "0px 0px 20px 11px rgba(0, 0, 0, 0.32)");
+            $(".mobile-interface3").css("left", "61vw");
+        }, 300);
     });
     
-    $(".mobile-interface").mouseenter(function (e){
+    $(".mobile-interface").mousemove(function (e) {
+        e.stopPropagation();
+    });
+    $(".mobile-interface").mouseenter(function (e) {
         $('#top-image').css("transition", "0.5s");
         $('#top-image').css("transform", "scale(1.5)");
     })
-    $(".mobile-interface").mouseleave(function (e){
-        
+    $(".mobile-interface").mouseleave(function (e) {
         $('#top-image').css("transform", "scale(1)");
         $('#top-image').css("transition", "0.1s");
     });
